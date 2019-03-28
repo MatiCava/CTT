1506fb2c9c454661e683c09319bdde717ca281e8
MatiCava <matiascavallin96@gmail.com>
2019-03-28 17:27:13 -0300

    Movimiento solo hacia delante

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Animator anim;
+    private string moveInputAxis = "Vertical";
+    private string turnInputAxis = "Horizontal";
+    public float rotationRate = 360;
+    public float movSpeed = 10;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveAxis = Input.GetAxis(moveInputAxis);
+        float turnAxis = Input.GetAxis(turnInputAxis);
+
+        ApplyInput(moveAxis, turnAxis);
+	}
+
+    private void ApplyInput(float moveInput, float turnInput)
+    {
+        move(moveInput);
+        turn(turnInput);
+    }
+
+    private void move(float input)
+    {
+        anim.SetFloat("Speed", input);
+        rb.AddForce(transform.forward * input * movSpeed, ForceMode.Force);
+    }
+
+    private void turn(float input)
+    {
+        transform.Rotate(0, input * rotationRate * Time.deltaTime, 0);
+    }
+}
